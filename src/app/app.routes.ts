8ea34ba54bd2b153fe8f7973eb01dbd5624cca88
Eryk <eryk9903@gmail.com>
2025-02-26 16:43:58 +0100

    create login and register templates

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,19 @@
 import { Routes } from '@angular/router';
 import { AddRecipeComponent } from './recipes/add-recipe/add-recipe.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
 
 export const routes: Routes = [
     {
         path: 'recipes',
         component: AddRecipeComponent
+    },
+    {
+        path: 'login',
+        component: LoginComponent
+    },
+    {
+        path: 'register',
+        component: RegisterComponent
     }
 ];
