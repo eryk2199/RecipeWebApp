b137bcd18da7e9b0e1de9f7a67567f05ba07babf
Eryk <eryk9903@gmail.com>
2025-02-27 11:42:29 +0100

    add login and register form validation

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'auth-login',
-  imports: [RouterLink],
+  imports: [RouterLink, FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  email = '';
+  password = ''
 
+  onSubmit(loginForm: NgForm) {
+    if(loginForm.invalid) {
+      loginForm.form.markAllAsTouched()
+      return;
+    }
+
+    console.log({
+      email: this.email,
+      password: this.password
+    })
+  }
 }
--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,11 +1,32 @@
 import { Component } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'auth-register',
-  imports: [],
+  imports: [RouterLink, FormsModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
+  email = '';
+  password = '';
+  confirmPassword = '';
+
+  onSubmit(registerForm: NgForm) {
+    if(registerForm.invalid) {
+      registerForm.form.markAllAsTouched()
+      return;
+    }
+    if(this.password !== this.confirmPassword) {
+      registerForm.form.get('confirmPassword')?.setErrors({noMatch: true});
+      return;
+    }
+    console.log({
+      email: this.email,
+      password: this.password
+    })
+  }
 
 }
+
